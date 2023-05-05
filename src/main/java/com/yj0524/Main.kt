18a04b9568303a81d9bbc4db42fb18e2d300b159
yj0524 <jeonyunjae_kr@naver.com>
2023-05-05 16:21:49 +0900

    kommand classification cancel

--- src/main/java/com/yj0524/Main.kt
+++ src/main/java/com/yj0524/Main.kt
@@ -1,5 +1,6 @@
 package com.yj0524
 
+import io.github.monun.kommand.*
 import org.bukkit.Material
 import org.bukkit.entity.Monster
 import org.bukkit.entity.Player
@@ -17,7 +18,6 @@ import org.bukkit.potion.PotionEffectType
 import java.io.File
 import java.util.*
 
-
 open class Main : JavaPlugin(), Listener {
 
     var banMode = false
@@ -34,7 +34,7 @@ open class Main : JavaPlugin(), Listener {
             saveConfig()
         }
 
-        EogkkaKommand().kommandLoad()
+        kommandLoad()
 
         server.pluginManager.registerEvents(this, this)
 
@@ -49,6 +49,79 @@ open class Main : JavaPlugin(), Listener {
 
     override fun onDisable() {
         server.logger.info("Plugin Disabled")
+    }
+
+    fun kommandLoad() {
+        val greedy = KommandArgument.string(StringType.GREEDY_PHRASE)
+
+        kommand {
+            register("eogkka") {
+                requires {
+                    isOp
+                }
+                executes {
+                    sender.sendMessage("§c사용법 : /eogkka <type>")
+                }
+                then("info") {
+                    executes {
+                        sender.sendMessage("Plugin Name : " + pluginMeta.name)
+                        sender.sendMessage("Plugin Version : " + pluginMeta.version)
+                        sender.sendMessage("Plugin API Version : " + pluginMeta.apiVersion)
+                    }
+                }
+                then("notice") {
+                    executes {
+                        sender.sendMessage("§c사용법 : /eogkka notice <message>")
+                    }
+                    then("message" to greedy) {
+                        executes {
+                            val message: String by it
+
+                            server.broadcastMessage("§a[억까 서바이벌 공지] §r" + message)
+                        }
+                    }
+                }
+                then("forcestop") {
+                    executes {
+                        saveConfig()
+                        server.shutdown()
+                    }
+                }
+                then("config") {
+                    executes {
+                        sender.sendMessage("§c사용법 : /eogkka config <type> <value>")
+                    }
+                    then("banmode") {
+                        executes {
+                            sender.sendMessage("§c사용법 : /eogkka config banmode <bool>")
+                        }
+                        then("bool" to bool()) {
+                            executes {
+                                val bool: Boolean by it
+
+                                banMode = bool
+                                loadConfig()
+                                sender.sendMessage("§a밴 모드를 $bool (으)로 설정했습니다.")
+                            }
+                        }
+                    }
+                    then("deathcount") {
+                        executes {
+                            sender.sendMessage("§c사용법 : /eogkka config deathcount <int>")
+                        }
+                        then("int" to int()) {
+                            executes {
+                                val int: Int by it
+
+                                deathCount = int
+                                loadConfig()
+                                sender.sendMessage("§a억까당한 횟수를 $int (으)로 설정했습니다.")
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     fun loadConfig() {
